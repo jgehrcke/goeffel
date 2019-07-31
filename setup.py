3720b779f69ca052fb9e1a873a7cf40ffa28487d
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2019-07-31 16:43:38 +0200

    PyPI project structure: add setup.py

---
+++ setup.py
@@ -0,0 +1,39 @@
+import re
+from setuptools import setup
+
+
+version = re.search(
+    '^__version__\s*=\s*"(.*)"',
+    open('schniepel/schniepel.py').read(),
+    re.M
+    ).group(1)
+
+
+with open('README.md', 'rb') as f:
+    long_descr = f.read().decode('utf-8')
+
+
+setup(
+    name = 'schniepel',
+    packages = ['schniepel'],
+    entry_points = {
+        'console_scripts': [
+            'schniepel = schniepel.schniepel:main',
+            'schniepel-analysis = schniepel.schniepel_analysis:main'
+            ]
+        },
+    version = version,
+    description = 'Measures the resource utilization of a specific process over time',
+    long_description = long_descr,
+    author = 'Dr. Jan-Philip Gehrcke',
+    author_email = 'jgehrcke@googlemail.com',
+    classifiers = [
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: POSIX',,
+        ],
+    install_requires=('tables', 'pandas', 'psutil'),
+    )
