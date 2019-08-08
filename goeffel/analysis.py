b38aa2f03ec2c49da74e775e9301aa68c16807d1
Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2019-08-08 18:30:26 +0200

    analysis: fix flake8 error

--- goeffel/analysis.py
+++ goeffel/analysis.py
@@ -69,7 +69,7 @@ COLUMN_PLOT_CONFIGS = {
         'plot_title': 'foo',
         'rolling_wdw_width_seconds': 0,
     },
-   'system_loadavg1': {
+    'system_loadavg1': {
         'y_label': 'System 1 min load avg',
         'plot_title': 'foo',
         'rolling_wdw_width_seconds': 0
