31244f8c7745bcd0e90aeb5ed592f26ae8cc86e5
Dr. Jan-Philip Gehrcke <jgehrcke@googlemail.com>
2023-06-02 10:55:43 +0200

    setup.py: reformat, change lang version specifiers

--- setup.py
+++ setup.py
@@ -3,43 +3,40 @@ from setuptools import setup
 
 
 version = re.search(
-    r'^__version__\s*=\s*"(.*)"',
-    open('goeffel/goeffel.py').read(),
-    re.M
-    ).group(1)
+    r'^__version__\s*=\s*"(.*)"', open("goeffel/goeffel.py").read(), re.M
+).group(1)
 
 
-with open('README.md', 'rb') as f:
-    long_descr = f.read().decode('utf-8')
+with open("README.md", "rb") as f:
+    long_descr = f.read().decode("utf-8")
 
 
 setup(
-    name='goeffel',
-    packages=['goeffel'],
+    name="goeffel",
+    packages=["goeffel"],
     entry_points={
-        'console_scripts': [
-            'goeffel = goeffel.goeffel:main',
-            'goeffel-analysis = goeffel.analysis:main'
-            ]
-        },
+        "console_scripts": [
+            "goeffel = goeffel.goeffel:main",
+            "goeffel-analysis = goeffel.analysis:main",
+        ]
+    },
     version=version,
-    description='Measures the resource utilization of a specific process over time',
+    description="Measures the resource utilization of a specific process over time",
     long_description=long_descr,
-    long_description_content_type='text/markdown',
-    author='Dr. Jan-Philip Gehrcke',
-    author_email='jgehrcke@googlemail.com',
-    url='https://github.com/jgehrcke/goeffel',
+    long_description_content_type="text/markdown",
+    author="Dr. Jan-Philip Gehrcke",
+    author_email="jgehrcke@googlemail.com",
+    url="https://github.com/jgehrcke/goeffel",
     classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Operating System :: POSIX',
-        ],
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Operating System :: POSIX",
+    ],
     # TODO(JP):pandas, matplotlib are for the analysis program only. Make it so
     # that the measurement program can be pip-installed w/o requiring pandas and
     # matplotlib. Use https://setuptools.readthedocs.io/en/latest/setuptools.html
     # #declaring-extras-optional-features-with-their-own-dependencies
-    install_requires=('tables', 'psutil', 'pandas', 'matplotlib'),
-    )
+    install_requires=("tables", "psutil", "pandas", "matplotlib"),
+)
